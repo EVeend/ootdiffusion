8d5d06b975088fb1e193b41a98ce624a200a05f9
Yuhao Xu <levihsu@users.noreply.huggingface.co>
2024-03-08 16:15:49 +0000

    Update ootd/inference_ootd.py

--- ootd/inference_ootd.py
+++ ootd/inference_ootd.py
@@ -25,10 +25,10 @@ import torch.nn.functional as F
 from transformers import AutoProcessor, CLIPVisionModelWithProjection
 from transformers import CLIPTextModel, CLIPTokenizer
 
-VIT_PATH = "../checkpoints/clip-vit-large-patch14"
-VAE_PATH = "../checkpoints/ootd"
-UNET_PATH = "../checkpoints/ootd/ootd_hd/checkpoint-36000"
-MODEL_PATH = "../checkpoints/ootd"
+VIT_PATH = "openai/clip-vit-large-patch14"
+VAE_PATH = "./checkpoints/ootd"
+UNET_PATH = "./checkpoints/ootd/ootd_hd/checkpoint-36000"
+MODEL_PATH = "./checkpoints/ootd"
 
 class OOTDiffusion:
 
